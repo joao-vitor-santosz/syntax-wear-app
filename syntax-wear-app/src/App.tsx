a8159bbed05eb3378fc45714ea8f8306ebe141b0
João Vítor <joaovns0812@gmail.com>
2026-06-18 12:56:07 -0400

    feat: adiciona componente Hero

--- syntax-wear-app/src/App.tsx
+++ syntax-wear-app/src/App.tsx
@@ -1,13 +1,15 @@
-import { Header } from "./components/Header"
-
+import { Header } from "./components/Header";
+import { Hero } from "./components/Hero";
 
 function App() {
-
   return (
     <>
       <Header />
+      <main className="py-10">
+        <Hero />
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
