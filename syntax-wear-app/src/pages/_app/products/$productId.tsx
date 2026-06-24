6d66b8e41f80967471730de73746314824313254
João Vítor <joaovns0812@gmail.com>
2026-06-24 10:29:10 -0400

    feat: adiciona context de produtos no carrinho

--- syntax-wear-app/src/pages/_app/products/$productId.tsx
+++ syntax-wear-app/src/pages/_app/products/$productId.tsx
@@ -1,17 +1,23 @@
 import { createFileRoute, Link } from "@tanstack/react-router";
 import { products } from "../../../mocks/products";
 import formatCurrency from "../../../utils/format-currency";
+import { useContext } from "react";
+import { CartContext } from "../../../contexts/CartContext";
 
 export const Route = createFileRoute("/_app/products/$productId")({
   component: RouteComponent,
 });
 
 function RouteComponent() {
+
+  const { addToCart } = useContext(CartContext)
   const { productId } = Route.useParams();
 
   const filteredProduct = products.find(
     (product) => product.id === Number(productId),
   );
+
+  if(!filteredProduct) return;
 
   const originalPrice = filteredProduct?.price ?? 0;
   const discountPrice = originalPrice * 0.9;
@@ -70,8 +76,7 @@ function RouteComponent() {
             </form>
           </div>
 
-          <button className="bg-black text-white rounded-md p-5 w-full cursor-pointer hover:bg-gray-800">Adicionar ao carrinho</button>
-
+          <button className="bg-black text-white rounded-md p-5 w-full cursor-pointer hover:bg-gray-800" onClick={() => addToCart(filteredProduct)}>Adicionar ao carrinho</button>
         </div>
       </div>
     </section>
