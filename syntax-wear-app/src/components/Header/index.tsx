9c57261581364489fdd91c478627d04168b3f379
João Vítor <joaovns0812@gmail.com>
2026-06-24 00:38:24 -0400

    feat: adiciona drawer do carrinho de compras

--- syntax-wear-app/src/components/Header/index.tsx
+++ syntax-wear-app/src/components/Header/index.tsx
@@ -3,6 +3,7 @@ import IconUser from "@/assets/images/icon-user.png";
 import IconAbout from "@/assets/images/icon-about.png";
 import IconCart from "@/assets/images/icon-cart.png";
 import { Link } from "@tanstack/react-router";
+import { ShoppingCart } from "../ShoppingCart";
 
 export const Header = () => {
   return (
@@ -46,9 +47,11 @@ export const Header = () => {
                 </a>
               </li>
               <li>
-                <a href="#">
+                {/* <a href="#">
                   <img src={IconCart} alt="Ícone de carrinho" />
-                </a>
+                </a> */}
+
+                <ShoppingCart />
               </li>
             </ul>
           </nav>
